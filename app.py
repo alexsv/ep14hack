d4b132a18b4df5c540eefd1d750930998c071e61
Alex Vasilenko <alex@localhost.com>
2014-07-24 19:11:26 +0300

    added static folder

--- app.py
+++ app.py
@@ -4,18 +4,23 @@ import os
 
 app = Flask(__name__)
 
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+
 @app.route("/")
 def index():
     return 'ok'
 
 @app.route("/routes")
 def routes():
-    route_path = './data/rk_berlin_routes/'
-    routes = [json.load(open(os.path.join(route_path, filename))) for filename in os.listdir(route_path)]
+    route_path = 'data/rk_berlin_routes/'
+    routes = [json.load(open(os.path.join(BASE_PATH, route_path, filename)))
+              for filename in os.listdir(route_path)]
     routes = [dict(id=i+1,
                    points=map(lambda x: dict(lat=x['latitude'], lon=x['longitude']), route)) 
               for i, route in enumerate(routes)]
     return jsonify(results=routes)
+
+app.static_folder   = os.path.join(BASE_PATH, 'static')
 
 if __name__ == '__main__':
     app.run(debug=True)
